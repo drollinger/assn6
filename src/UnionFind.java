b74d6b562a24a4dff40b919a607cad705d15bac6
drollinger <drollingerdallin@gmail.com>
2019-04-16 13:25:20 -0600

    Finished the union find

--- src/UnionFind.java
+++ src/UnionFind.java
@@ -1,3 +1,62 @@
 public class UnionFind {
-    
+
+    private int[] setList;
+
+    /**********************************
+     * Constructor to set up the original number of sets
+     **********************************/
+    public UnionFind(int listSize) {
+        this.setList = new int[listSize];
+        for(int i = 0; i < setList.length; i++) {
+            setList[i] = -1;
+        }
+    }
+
+    /**********************************
+     * Unionizes two roots
+     **********************************/
+    public void union(int root1, int root2) {
+        if(this.setList[root1] < this.setList[root2]) {
+            this.setList[root1] += this.setList[root2];
+            this.setList[root2] = root1;
+        }
+        else {
+            this.setList[root2] += this.setList[root1];
+            this.setList[root1] = root2;
+        }
+    }
+
+    /**********************************
+     * finds and returns value at location given.
+     * If not found it returns the location.
+     **********************************/
+    public int find(int location) {
+        if(this.setList[location] < 0){
+            return location;
+        }
+        else {
+            return this.setList[location] = (this.setList[location]);
+        }
+    }
+
+    public void printArray() {
+        System.out.println("Item #:\t| Parent/Size: ");
+        System.out.println("========|============");
+        for(int i = 0; i < this.setList.length; i++)
+            System.out.println(i + "\t\t|\t" + this.setList[i]);
+    }
+
+    public static void main(String[] args) {
+        UnionFind unionFindTest = new UnionFind(30);
+        System.out.println("Array initialized...");
+        unionFindTest.printArray();
+        System.out.println("Building up the first two sets with 2 and 4, and 6 and 8...");
+        unionFindTest.union(unionFindTest.find(2), unionFindTest.find(4));
+        unionFindTest.union(unionFindTest.find(6), unionFindTest.find(8));
+        unionFindTest.printArray();
+        System.out.println("Adding 3 to the 2 set and combining it with the 6 set...");
+        unionFindTest.union(unionFindTest.find(2), unionFindTest.find(3));
+        unionFindTest.union(unionFindTest.find(2), unionFindTest.find(6));
+        unionFindTest.printArray();
+    }
 }
